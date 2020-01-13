80f32f3f68b5191a40c0c43485c686396a019553
Esteban Garces <AnthonyGarces@users.noreply.github.com>
2020-01-13 15:36:31 -0600

    Added script to display requested individual tables, also added script to restart or end the program

--- server.js
+++ server.js
@@ -22,27 +22,88 @@ var connection = mysql.createConnection({
     start();
   });
 
-  function start() {
-      inquirer
+function start() {
+    inquirer
+      .prompt([
+        {
+            type: 'list',
+            name: "request",
+            message: "What would you like to do?",
+            choices: [
+                'View all employees',
+                'View all employees by department',
+                'View all employees by Manager',
+                'Add Employee',
+                'Remove Employee',
+                'Update employee role',
+                'Update employee Manager',
+                'View all roles',
+                'Add Role',
+                'Remove role',
+                'View departments',
+                'Add departments',
+                'Remove departments'
+            ],           
+        }
+    ])
+    .then(answers => {
+        switch (answers.request) {
+        case "View all employees":
+            getEmployees();
+            break;
+        
+        case "View all roles":
+            getRoles();
+            break;
+        
+        case "View departments":
+            getDepartments();
+            break;
+        
+        }
+    })       
+}
+
+function getEmployees() {
+connection.query("SELECT * FROM employee", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    newRequest()
+})
+}
+
+function getRole() {
+connection.query("SELECT * FROM role", function(err, res) {
+    if (err) throw err;
+    console.table(res)
+})
+}
+
+function getDepartments() {
+    connection.query("SELECT * FROM department", function(err, res) {
+        if (err) throw err;
+        console.table(res)
+    })
+}
+
+function newRequest() {
+    inquirer
         .prompt([
             {
-                type: 'list',
-                name: "request",
-                message: "What would you like to do?",
-                choices: [
-                    'View all employees',
-                    'View all employees by department',
-                    'View all employees by Manager',
-                    'Add Employee',
-                    'Remove Employee',
-                    'Update employee role',
-                    'Update employee Manager',
-                    'View all roles',
-                    'Add Role',
-                    'Remove role'
-                ],           
+                type: 'confirm',
+                name: 'Continue',
+                message: "Do you have another request?",
             }
         ])
-        .then(answers =>
-            console.log(answers.request))
-  }
+        .then(data => {
+            switch (data.Continue) {
+                case false:
+                    console.log("Thanks for using our service!");
+                    connection.end();
+                    break;
+
+                    case true:
+                        start()
+            }
+        })
+}
